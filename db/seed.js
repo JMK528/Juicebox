38c45863194081467effd79a516fcf90b4842cf5
Justin Kerr <justinmk1@comcast.net>
2022-09-22 18:08:54 -0600

    fixed project refresh issue

--- db/seed.js
+++ db/seed.js
@@ -43,8 +43,8 @@ async function createInitialUsers() {
     console.log("Starting to create users...");
 
     await createUser({ username: 'albert', password: 'bertie99' });
-    await createUser({ username: 'sandra', password: '2sandy4me' });
-    await createUser({ username: 'glamgal', password: 'soglam' });
+    // await createUser({ username: 'sandra', password: '2sandy4me' });
+    // await createUser({ username: 'glamgal', password: 'soglam' });
 
     console.log("Finished creating users!");
   } catch (error) {
